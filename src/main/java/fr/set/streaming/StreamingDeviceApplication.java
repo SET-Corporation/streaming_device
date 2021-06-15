0cdd162183766dc0e4795fddd246214eb1232895
Julien Dumez <jdumez@set-sas.fr>
2021-06-15 16:26:23 +0200

    clean up imports

--- src/main/java/fr/set/streaming/StreamingDeviceApplication.java
+++ src/main/java/fr/set/streaming/StreamingDeviceApplication.java
@@ -1,6 +1,5 @@
 package fr.set.streaming;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
