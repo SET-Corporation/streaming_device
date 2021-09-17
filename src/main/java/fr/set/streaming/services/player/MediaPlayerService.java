c04e853340dcf8485f5043a0626b46b351914a74
Julien Dumez <jdumez@set-sas.fr>
2021-09-17 13:13:57 +0200

    config: remove omx option

--- src/main/java/fr/set/streaming/services/player/MediaPlayerService.java
+++ src/main/java/fr/set/streaming/services/player/MediaPlayerService.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
-//import uk.co.caprica.vlcj.player.embedded.windows.Win32FullScreenStrategy;
-import uk.co.caprica.vlcj.player.embedded.x.XFullScreenStrategy;
+import uk.co.caprica.vlcj.player.embedded.windows.Win32FullScreenStrategy;
+//import uk.co.caprica.vlcj.player.embedded.x.XFullScreenStrategy;
 
 import java.awt.*;
 
@@ -23,8 +23,8 @@ public class MediaPlayerService {
 
     public EmbeddedMediaPlayer getNewGraphicalEmbeddedMediaPlayer() {
         Canvas canvas = graphicalService.getCanvas();
-        EmbeddedMediaPlayer embeddedMediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer(new XFullScreenStrategy(graphicalService.getJframe(canvas)));
-        //EmbeddedMediaPlayer embeddedMediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer(new Win32FullScreenStrategy(graphicalService.getJframe(canvas)));
+        //EmbeddedMediaPlayer embeddedMediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer(new XFullScreenStrategy(graphicalService.getJframe(canvas)));
+        EmbeddedMediaPlayer embeddedMediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer(new Win32FullScreenStrategy(graphicalService.getJframe(canvas)));
         embeddedMediaPlayer.setVideoSurface(mediaPlayerFactory.newVideoSurface(canvas));
         embeddedMediaPlayer.toggleFullScreen();
         embeddedMediaPlayer.setEnableKeyInputHandling(false);
